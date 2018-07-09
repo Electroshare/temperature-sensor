6f3a87d63e33549811ffedc36cb2a4da544afe26
Pierre Cadart <pierre.cadart@supelec.fr>
2018-07-09 12:52:41 +0200

    Added licence notice to the code

--- code/sensor_temp_attiny/sensor_temp_attiny.ino
+++ code/sensor_temp_attiny/sensor_temp_attiny.ino
@@ -4,7 +4,24 @@
  * UPDATE : 01-01-2018
  * rev : 1.2
  * Louis Barbier
+ *
+ * This file is part of Temperature-sensor.
+ *
+ * Temperature-sensor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * Temperature-sensor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Temperature-sensor.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
+
 /*
 TRAME : [PARITY | ID_SENSOR | DECIMAL_TEMP | SIGN | INTEGER_TEMP]
         16      15         12              8      7             0
